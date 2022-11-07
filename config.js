7b77d80c2889a4a6c76af02eb2bfdd3d1f161ab4
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2022-11-07 18:19:27 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+<!-- Firebase -->
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-database.js"></script>
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDnzFqz6q8hj-WImI8qqrN7KcpfngOmzoU",
+    authDomain: "multiplayer-car-racing-g-e094a.firebaseapp.com",
+    databaseURL: "https://multiplayer-car-racing-g-e094a-default-rtdb.firebaseio.com",
+    projectId: "multiplayer-car-racing-g-e094a",
+    storageBucket: "multiplayer-car-racing-g-e094a.appspot.com",
+    messagingSenderId: "206591446626",
+    appId: "1:206591446626:web:11b3a95c7b1e65235a8b3b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
